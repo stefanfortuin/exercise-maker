c14f4ff17ae001dc66b5796be9c0b9b2dc23c02b
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-04-07 15:16:08 +0200

    laravel ui install for auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,16 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .webpackConfig({
+		devServer: {
+			host: '0.0.0.0',
+			port: 8080,
+		},
+	})
+	.options({
+		hmrOptions: {
+			host: 'localhost',
+			port: 8080,
+		},
+	});
